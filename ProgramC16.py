829342f4855acb0ab2d7dd8939a81ff0615fa516
NobleSpittoon <105742922+NobleSpittoon@users.noreply.github.com>
2022-05-17 12:50:33 -0500

    Update ProgramC16.py

--- ProgramC16.py
+++ ProgramC16.py
@@ -1,3 +1,5 @@
 print("This program will get the sum price of all items you input into it.")
 print("Type None as the name of an item to end the program.")
 print("Type $0 as the price of an item to end the program.")
+want_to_run_program = 'y'
+while want_to_run_program == 'y':
