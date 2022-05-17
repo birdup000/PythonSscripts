5276c5864787b6d3b35edb280137cfcc5c31eb10
NobleSpittoon <105742922+NobleSpittoon@users.noreply.github.com>
2022-05-17 13:06:34 -0500

    C

--- ProgramC16.py
+++ ProgramC16.py
@@ -7,5 +7,16 @@ while want_to_run_program == 'y':
     item_prices = []
     want_to_add_items = 'y'
     while True:
-        New_Name = input("What is the name of your first item?")
-        
+        New_Name = input("What is the name of this item?")
+        if New_Name == 'None':
+            break
+        else:
+            item_names.append(New_Name)
+        New_Price = float(int(input("What is the price of this item? (please don't use a $ symbol)")))
+        New_Price = round(New_Price, 2)
+        if New_Price == '0':
+            item_names.pop
+            break
+        else:
+            item_names.append(New_Price)
+    allitems = item_names
