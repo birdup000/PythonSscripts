06bf3180293c45f045f40fff4e4d39c62e0f2902
NobleSpittoon <105742922+NobleSpittoon@users.noreply.github.com>
2022-05-17 12:48:44 -0500

    Update ProgramC16.py

--- ProgramC16.py
+++ ProgramC16.py
@@ -1,1 +1,3 @@
-#This is a comment
+print("This program will get the sum price of all items you input into it.")
+print("Type None as the name of an item to end the program.")
+print("Type $0 as the price of an item to end the program.")
