049e127d9ec07bbf8ffe85e04695976d0866c761
NobleSpittoon <105742922+NobleSpittoon@users.noreply.github.com>
2022-05-17 12:54:47 -0500

    Update ProgramC16.py

--- ProgramC16.py
+++ ProgramC16.py
@@ -3,3 +3,9 @@ print("Type None as the name of an item to end the program.")
 print("Type $0 as the price of an item to end the program.")
 want_to_run_program = 'y'
 while want_to_run_program == 'y':
+    item_names = []
+    item_prices = []
+    want_to_add_items = 'y'
+    while True:
+        New_Name = input("What is the name of your first item?")
+        
