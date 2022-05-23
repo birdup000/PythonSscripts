f278727957812b47265df16319d6c00b5f7c0e9a
Bullseyecowbelly <34012548+greengeckowizard@users.noreply.github.com>
2022-05-23 12:02:13 -0500

    UPGRADE :smile:
    this is a adding more stuff to the python files :+1: and a lot more will be added

--- ProgramC7.py
+++ ProgramC7.py
@@ -1,25 +1,42 @@
 age = int(input("What age are you?"))
 
 
-
-if age < 5:
-    print("You are in preschool.")
-
-
-if age > 5 or age <= 6:
-    print("You are in Kindergarten.")
-
-    if age >= 7 or age <=10:
-     print("You are in Elementary school.")
+if (age < 5):
+    print("You're in Preschool.")
+elif (age > 5 or age <= 6):
+      print("You're in Kindergarten.")
 
 
-if age  >= 11 or age <=14: 
-    print("You are in Middle school.")
+if (age >= 7 or age <=10):
+    print("You are in Elementary school.")
+elif (age  >= 11 or age <=14):
+      print("You are in Middle school.")
+
+    
 
 
 
-if age >= 15 or age <=17:
-    print("You are in High school.")
 
-if age >=18:
-    print("You are in College.")
+
+
+
+
+###if age < 5:
+###    print("You are in preschool.")
+
+
+###if age > 5 or age <= 6:
+###   print("You are in Kindergarten.")
+
+###    if age >= 7 or age <=10:
+###     print("You are in Elementary school.")
+
+
+###if age  >= 11 or age <=14: 
+###    print("You are in Middle school.")
+
+###if age >= 15 or age <=17:
+###    print("You are in High school.")
+
+###if age >=18:
+###    print("You are in College.")
--- ProgramC9.py
+++ ProgramC9.py
@@ -1,18 +1,44 @@
-daynumber = 0
-askmilage = int (input("How many miles did you travel day"))
-Answer = input('Want to see the milage ?')
-tmiles = 0
-Milage = 0
-while Answer == 'yes' or Answer == 'y':
+totalteammember = 0
+teammember1 = 0
+teammember2 = 0
+teammember3 = 0
+teammember4 = 0
+teammember5= 0
+#TODO Create a while loop looping around up to 5 times for each team
+
+teammember1 = int(input("What's your milage? "))
+print("Your mileage is ",teammember1)
+##correct way of adding with the = not the == and also the variable in front instead of behind
+totalteammember= teammember1 + teammember2 + teammember3 + teammember4 + teammember5
+print("The current total team milage:",totalteammember)
+
+
+teammember2 = int(input("Enter your milage here "))
+print("your mileage",teammember2) 
+totalteammember = teammember1 + teammember2 + teammember3 + teammember4 + teammember5  
+print("The current total team milage:",totalteammember)
+
+
+
+
+teammember3 = int(input("Enter your milage here "))
+print("your mileage",teammember3)
+teammember1 + teammember2 + teammember3 + teammember4 + teammember5 == totalteammember 
+
+print("The current total team milage:",totalteammember)
+
+
+
+teammember4 = int(input("Enter your milage here "))
+print("your mileage",teammember5)
+teammember1 + teammember2 + teammember3 + teammember4 + teammember5 == totalteammember 
+
+print("The current total team milage:",totalteammember)
+
+
+teammember5 = int(input("Enter your milage here "))
+print("your mileage",teammember5)
+teammember1 + teammember2 + teammember3 + teammember4 + teammember5 == totalteammember 
+
+print("The current total team milage:",totalteammember)
    
-    print("The program will give you or team milage number",askmilage,"degrees" )
-    if (Milage > 1 ): 
-        Milage == tmiles
-        print("here's your Milage")
-    elif (Milage > 1 ):
-        print("Your team miles is", tmiles)
-    else:
-        print("Would you like to add more team data?") 
-    daynumber = daynumber + 1
-    Answer = int(input("How many team miles were ran?"))
-print('Have a great day!')
